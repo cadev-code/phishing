2266549e306cd6c2052751afa0a5e3a42762ff86
Carlos Alberto <carlosdev1810@gmail.com>
2025-09-08 20:16:30 -0600

    feat(server): add create register query
    - Build validateRegisterInput middleware to validate input data.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,33 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { pool } from './db';
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+const validateRegisterInput = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (
+    !req.body.email ||
+    typeof req.body.email !== 'string' ||
+    !req.body.email.includes('@')
+  ) {
+    res.status(400).send({ error: 'Correo electrónico invalido.' });
+    return;
+  }
+  next();
+};
+
+app.post('/register', validateRegisterInput, (req: Request, res: Response) => {
+  const { email } = req.body;
+  pool.query('INSERT INTO registers (email) VALUES (?)', [email]);
+  res.status(200).send({ message: 'Correo registrado correctamente.' });
+});
 
 app.listen(PORT, () => {});
